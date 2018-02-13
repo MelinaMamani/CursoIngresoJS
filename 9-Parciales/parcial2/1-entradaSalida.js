30fa677c64be91b7b3606c9fce485615aa20ecb5
MelinaMamani <erikamelinamamani@gmail.com>
2018-02-13 16:59:22 -0300

    modificados 13/02

--- 9-Parciales/parcial2/1-entradaSalida.js
+++ 9-Parciales/parcial2/1-entradaSalida.js
@@ -1,7 +1,11 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	alert("Funciona 1-EntradaSalida");
+	var base, perimetro;
+	base= parseInt(document.getElementById("lado").value);
+	perimetro= base*3;
+
+	alert("Perímetro = "+perimetro);
 	
 }
 
