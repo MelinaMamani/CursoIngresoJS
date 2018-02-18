d68ebb273c46d27342efb2230ec230dac14a2600
MelinaMamani <erikamelinamamani@gmail.com>
2018-02-18 00:19:28 -0300

    modificado 18/02

--- 8-TPs/jsAdivinaElNumero1.js
+++ 8-TPs/jsAdivinaElNumero1.js
@@ -10,7 +10,6 @@ de no ser igual se debe informar si “falta…”  para llegar al número secre
 
 /*Para la clase que viene los intentos no deben ser mayor a 10 y no debe acertar en el primer intento (trampa)*/
 
-
 var numeroSecreto; 
 var numero, falta, pasa;
 var contadorIntentos= 0;
@@ -18,14 +17,14 @@ var contadorIntentos= 0;
 function comenzar()
 {
 	numeroSecreto= Math.floor((Math.random()*(101-1))+1);
-  console.log(numeroSecreto);
+  	console.log(numeroSecreto);
 }
 
-function verificar()
+/*function verificar()
 {
   numero= document.getElementById("numero").value;
 
-	if (numero==numeroSecreto) {
+  if (numero==numeroSecreto) {
     contadorIntentos++;
     alert("Acertaste!");
     document.getElementById("intentos").value = contadorIntentos;
@@ -41,5 +40,36 @@ function verificar()
     contadorIntentos++;
     alert("Te falta "+falta);
     document.getElementById("intentos").value = contadorIntentos;
+  }*/
+function verificar()
+{
+  numero= document.getElementById("numero").value;
+
+  if(contadorIntentos<10){
+  	
+  	contadorIntentos++;
+    
+  	if (numero==numeroSecreto) {
+      
+      if (contadorIntentos==1) {
+      	alert("Sigue participando...");
+      }
+      else{
+      	alert("Acertaste!");
+      }
+  	}
+    else if(numero>numeroSecreto){
+      pasa= numero - numeroSecreto;
+      alert("Se pasó por "+pasa);
+    }
+    else if(numero<numeroSecreto){
+      falta= numeroSecreto- numero;
+      alert("Te falta "+falta);
+     }
+    document.getElementById("intentos").value = contadorIntentos;
+    
+  }
+  else if (contadorIntentos>=10) {
+  	alert("Ya no te quedan mas intentos.");
   }
 }
