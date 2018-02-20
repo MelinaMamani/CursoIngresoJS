449fc042aa2061a96dc3ed2953ca809c030b82e0
MelinaMamani <erikamelinamamani@gmail.com>
2018-02-20 12:08:26 -0300

    modificados 20/02 12:07

--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -1,12 +1,38 @@
 function Mostrar()
 {
-
-	var contador=0;
 	var positivo=0;
 	var negativo=1;
-	
-	var respuesta='si';
+	var numero;
+	var respuesta='s';
 
+	while(respuesta == 's'){
+		numero= prompt("Ingrese un número", "");
+		if (numero == "") {
+			alert("Este campo está vacío");
+			continue;
+		}
+		numero = parseInt(numero);
+		while(isNaN(numero)) {
+				alert("Esto no es un numero");
+				break;
+			}
+		if (numero>0) {
+			positivo+=numero;
+		}
+		else{
+			if (numero!=0) {
+				negativo*=numero;
+			}
+		}
+		respuesta = prompt("Desea continuar? s/n","");
+			
+			if (respuesta == 'n') {
+				break;
+				}
+			else if (respuesta == null) {
+				break;
+			}
+	}
 
 document.getElementById('suma').value=positivo;
 document.getElementById('producto').value=negativo;
--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -1,18 +1,49 @@
 function Mostrar()
 {
-
+	var min= 0, numero, max= -1000, primera= true;
 	var contador=0;
 	// declarar variables
 	
-	var respuesta='si';
+	var respuesta='s';
 
-	while(respuesta!='no')
+	while(respuesta!='n')
 	{
+		numero = prompt("Ingrese un numero","");
+		if (numero == "") {
+			alert("Este campo está vacío");
+			continue;
+		}
+		numero = parseInt(numero);
+		if (isNaN(numero)==true) {
+				alert("Esto no es un numero");
+				break;
+			}
 		
+		if(primera){
+			primera= false;
+			max=numero;
+			min=numero;
+		}
+		else{
+			if (numero>max) {
+				max=numero;
+			}
+			else if(numero<min){
+				min=numero;
+			}
+		}
+		respuesta = prompt("Desea continuar? s/n","");
+			
+			if (respuesta == 'n') {
+				break;
+				}
+			else if (respuesta == null) {
+				break;
+			}
 	
 	}
 
-
-
+document.getElementById('maximo').value=max;
+document.getElementById('minimo').value=min;
 
 }//FIN DE LA FUNCIÓN
