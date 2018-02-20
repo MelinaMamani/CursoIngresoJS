9940099e2142075cdeeefaf2b5378ff2f26b00f8
MelinaMamani <erikamelinamamani@gmail.com>
2018-02-20 11:13:24 -0300

    modificados 20/02 11:08

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -1,12 +1,20 @@
 function Mostrar()
 {
-
+	var numero;
 	var contador=0;
 	var acumulador=0;
 
 
+	while (contador<5) {
+		contador++;
+		numero = prompt("Ingrese un numero","");
+		numero = parseInt(numero);
+		acumulador += numero;
+	}
 
 document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/5;
+var resultado;
+resultado = acumulador/contador;
+document.getElementById('promedio').value=resultado;
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -3,7 +3,43 @@ function Mostrar()
 
 	var contador=0;
 	var acumulador=0;
-	var respuesta='si';
+	var respuesta='s', mensaje;
+
+	/*while (respuesta != false) {
+		contador++;
+		numero = prompt("Ingrese un numero","");
+		numero = parseInt(numero);
+		acumulador += numero;
+		respuesta = confirm("Desea continuar?");
+			
+			if (respuesta == false) {
+				break;
+				}
+}*/
+	while (respuesta != 'n') {
+		numero = prompt("Ingrese un numero","");
+		if (numero == "") {
+			alert("Este campo está vacío");
+			continue;
+		}
+		numero = parseInt(numero);
+		if (isNaN(numero)==true) {
+				alert("Esto no es un numero");
+				break;
+			}
+		acumulador += numero;
+		contador++;
+		respuesta = prompt("Desea continuar? s/n","");
+			
+			if (respuesta == 'n') {
+				break;
+				}
+			else if (respuesta == null) {
+				break;
+			}
+			
+}
+	 
 
 
 document.getElementById('suma').value=acumulador;
