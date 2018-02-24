5fbd5f3e51b331f080bb04c221c79ccee04e2a14
MelinaMamani <erikamelinamamani@gmail.com>
2018-02-24 12:51:57 -0300

    modificado 24/02 do while 12:54

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -3,8 +3,11 @@ function Mostrar()
 
 var clave = prompt("Ingrese el número clave.");
 
-while (clave != "utn750" && clave!= null) {
+/*while (clave != "utn750" && clave!= null) {
    clave = prompt("Reingrese la clave.");
-}
+}*/
+do{
+    clave = prompt("Reingrese la clave.");
+ }while (clave != "utn750" && clave!= null);
 
 }//FIN DE LA FUNCIÓN
