6440787fd54e71871d52a06e043cf2987db6dd41
MelinaMamani <erikamelinamamani@gmail.com>
2018-02-26 08:53:39 -0300

    modificados 24/02 9:40

--- 6-InstruccionFor/jsIteracionesFor (3).js
+++ 6-InstruccionFor/jsIteracionesFor (3).js
@@ -1,8 +1,16 @@
 function Mostrar()
 {
+var repeticiones = prompt("ingrese el número de repeticiones");
+repeticiones= parseInt(repeticiones);
+while(isNaN(repeticiones))
+{
+    repeticiones = prompt("ingrese el número de repeticiones");
+    repeticiones= parseInt(repeticiones);
+}
 
-var repetciones = prompt("ingrese el número de repeticiones");
-
-
+for(var i = 0; i != repeticiones ; i++)
+{
+    alert("Hola utn fra");
+}
 
 }//FIN DE LA FUNCIÓN
