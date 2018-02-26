b8857e7d7bba3460627e1b704e78200f9037723e
MelinaMamani <erikamelinamamani@gmail.com>
2018-02-26 12:19:53 -0300

    modificado 26/02 12:20 para la próxima clase traerlo terminado

--- 8-TPs/jsRisingBTL.js
+++ 8-TPs/jsRisingBTL.js
@@ -11,34 +11,40 @@ F.	Nacionalidad, “A” para argentinos, “E” para extranjeros, “N” para
  */
 function ComenzarIngreso () 
 {
- var edad, sexo, sueldo, eCivil, legajo, nac;
+//Variables 1ra parte
+var edad, sexo, sueldo=8000, eCivil, legajo, nac;
+//variables 2da parte
+var maxNac=1, maxFemAr=1, promM=0, promF=0, promG=0, cantPerM=0, edadFem=0;
+var legajoArMenor=0, cantPerNeto, cantPerBruto;  
  
-   edad = prompt("Ingrese su edad","");
-   edad = parseInt(edad);
+   for (var num = 2; num >0; num--){
+       
+       /*edad = prompt("Ingrese su edad","");
+       edad = parseInt(edad);*/
    
    do {
-        if (edad==null) {
+         /*while (edad<18 || edad>90) {
             edad = prompt("Reingrese su edad","");
             edad = parseInt(edad);
             }
-         while (isNaN(edad)) {
+         while (isNaN(edad) || edad==null) {
             edad = prompt("Error. Reingrese su edad","");
             edad = parseInt(edad);
             }
          console.log(edad);
-         
+         */
          sexo = prompt("Ingrese f ó m .");
 
-         while ((sexo!="f" && sexo!="m") && (sexo!="F" && sexo!="M")) {
+         while (sexo.toLowerCase() !="f" && sexo.toLowerCase() !="m") {
                 sexo = prompt("Reingrese sexo f ó m","");
                 
-                if (sexo==null) {
+                while (sexo==null) {
                     sexo = prompt("Reingrese sexo f ó m","");
                 }
             }
         console.log(sexo);
         
-        alert(' A continuación elija una opción: \n 1) Soltero \n 2) Casado \n 3) Divorciado \n 4) Viudo') 
+        /*alert(' A continuación elija una opción: \n 1) Soltero \n 2) Casado \n 3) Divorciado \n 4) Viudo') 
         
         eCivil = prompt("Tu estado civil: ");
 
@@ -76,21 +82,88 @@ function ComenzarIngreso ()
                 eCivil = prompt("Reingresa tu estado civil: ");
                 break;
         }
+*/
+        sueldo = prompt("Ingrese su sueldo: ","");
+        sueldo = parseInt(sueldo);
+
+        while (sueldo<8000) {
+            sueldo = prompt("Reingrese un sueldo mayor a 8000","");
+            sueldo = parseInt(sueldo);
+            }
+        while (isNaN(sueldo) || sueldo==null) {
+            sueldo = prompt("Error al ingresar. Intente otra vez","");
+            sueldo = parseInt(sueldo);
+            }
+
+        /*legajo = prompt("Ingrese su número de legajo","");
+        legajo = parseInt(legajo);
+
+        while (legajo<1000 || legajo>9999) {
+            legajo = prompt("Reingrese su número de legajo","");
+            legajo = parseInt(legajo);
+        }
+        while (isNaN(legajo) || legajo==null) {
+            legajo = prompt("Error al ingresar. Intente otra vez","");
+            legajo = parseInt(legajo);
+            }*/
+        
+        alert(' Elija una opción: \n A) Argentino \n E) Extranjero \n N) Nacionalizado') 
+        
+        nac = prompt("Tu nacionalidad: ");
+
+        switch (nac) {
+            case 'A':
+            case 'a':
+                if ((sexo=="f" || sexo=="F") && sueldo>maxFemAr) {
+                   maxFemAr=sueldo;
+                   console.log(maxFemAr);
+                }
+                nac = "Argentina";
+                if (sexo=="M" || sexo=="m") {
+                    nac="Argentino";
+                }
+                break;
+            
+            case 'e':
+            case 'E':
+                nac = "Extranjera";
+                if (sexo=="M" || sexo=="m") {
+                    nac="Extranjero";
+                }
+                break;
+
+            case 'n':
+            case 'N':
+                if (sueldo>maxNac) {
+                   maxNac=sueldo;
+                   console.log(maxNac);
+                }
+
+                nac = "Nacionalizado";
+                if (sexo=="M" || sexo=="m") {
+                    nac="Nacionalizado";
+                }
+                break;
+        
+            default:
+                alert("Opción no válida.");
+                nac = prompt("Reingresa tu nacionalidad: ");
+                break;
+        }
 
     } while (edad<18 && edad>90);
 
-    if (edad>90) {
-            alert("Su edad es mayor a 90");
-        }
-    
     if (sexo=="f" || sexo=="F") {
         sexo="Femenino";
     } else if (sexo=="m" || sexo=="m"){
         sexo="Masculino";
     }
-    
+   }
 
     document.getElementById("Edad").value = edad;
     document.getElementById("Sexo").value = sexo;
     document.getElementById("EstadoCivil").value = eCivil;
+    document.getElementById("Sueldo").value = maxNac;
+    document.getElementById("Legajo").value = legajo;
+    document.getElementById("Nacionalidad").value = nac;
 }
